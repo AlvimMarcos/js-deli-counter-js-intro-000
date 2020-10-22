bc571fd2fbd76b8b1fd55f6a135b7ef4606c241a
Marcos Alvim <alvim.marcos@gmail.com>
2020-10-22 00:57:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,21 +14,15 @@ function nowServing(katzDeliLine) {
   }
 }
 
-function currentLine () {
-
-}
-
 function currentLine(katzDeliLine) {
-  var output = [];
-  if (katzDeliLine.length > 0) {
-    for (var i = 0; i < katzDeliLine.length; i++){
-      output +=(i+1) + ". " + katzDeliLine[i] + ", ";
-  }
-
-
-  output = output.slice(0; output.length -2);
-  return 'The line is currently: ' + output;
-}else {
-  return 'The line is currently empty.';
-}
+    var output = [];
+    if (katzDeliLine.length > 0) {
+      for(var i = 0; i < katzDeliLine.length; i++) {
+        output += (i + 1) + ". " + katzDeliLine[i] + ", ";
+      }
+      output = output.slice(0, output.length - 2);
+      return "The line is currently: " + output;
+    } else {
+        return "The line is currently empty.";
+    }
 }
